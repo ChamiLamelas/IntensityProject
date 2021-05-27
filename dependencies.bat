cb4de5215661078eac844f0ee54916bbc0a101dc
ChamiLamelas <chami.lamelas@gmail.com>
2021-05-26 23:19:44 -0400

    dependencies install change

--- dependencies.bat
+++ dependencies.bat
@@ -1,7 +1,7 @@
 @echo off
-pip install numpy
-pip install pandas
-pip install pillow
+pip install numpy --disable-pip-version-check
+pip install pandas --disable-pip-version-check
+pip install pillow --disable-pip-version-check
 pip list | findstr numpy
 pip list | findstr pandas
 pip list | findstr Pillow
